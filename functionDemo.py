c855179a0795a5a6ca1859833d468a3013a7a450
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-03-09 08:28:16 -0500

    Updated from Brython Server: 3/9/2018 8:28:15 AM

--- functionDemo.py
+++ functionDemo.py
@@ -4,3 +4,23 @@
 
 def hw():
     print('Hello, World')
+    
+def double(thingToDouble):
+    print(thingToDouble*2)
+
+def bigger(a,b):
+    if a>b:
+        print(a)
+    else:
+        print(b)
+
+def slope(x1, y1, x2, y2):
+    print((y2-y1)/(x2-x1))
+
+slope(2,3,4,5)
+bigger(100,5)
+bigger('smedinghoff','christo')
+bigger(True,False)
+#double(12) #test of double function
+#double('w') #test of double with string
+#double(True) #test of double with a boolean
