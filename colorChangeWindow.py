69a1cf2c5fa9ac3313022f398d866454164c35e0
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-03-09 08:48:33 -0500

    Updated from Brython Server: 3/9/2018 8:48:32 AM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -1,1 +1,16 @@
+#Liam Collins
+#3/9/18
+#colorChangeWindow.py
 
+from random import randint
+from ggame import *
+
+num = randint(1,3)
+
+red = Color(0xFF0000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+
+def mouseClick(event):
+    
+    
