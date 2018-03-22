d1e0e01481070b5150cea2ad590c46d38c8b9f52
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-03-22 08:30:49 -0400

    Updated from Brython Server: 3/22/2018 8:30:48 AM

--- monkeybanana.py
+++ monkeybanana.py
@@ -5,26 +5,43 @@
 from ggame import *
 
 #constants
-ROWS = 25
-COLS = 50
+ROWS = 20
+COLS = 40
 CELL_SIZE = 25
 
 def moveRight(event):
     monkey.x += CELL_SIZE
+    if monkey.x == banana.x and monkey.y == banana.y:
+        print('got it')
+
+def moveLeft(event):
+    monkey.x -= CELL_SIZE
+
+def moveDown(event):
+    monkey.y += CELL_SIZE
+    
+def moveUp(event):
+    monkey.y -= CELL_SIZE
 
 if __name__ == '__main__':
     
     #colors
     green = Color(0x006600,1)
     brown = Color(0x8B4513,1)
+    yellow = Color(0xFFFF00,1)
     
     jungleBox = RectangleAsset(CELL_SIZE*COLS,CELL_SIZE*ROWS,LineStyle(1,green),green)
     monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown),brown)
+    bananaBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,yellow),yellow)
     
     Sprite(jungleBox)
     monkey = Sprite(monkeyBox)
+    banana = Sprite(bananaBox,(CELL_SIZE*COLS/2,CELL_SIZE*ROWS/2))
     
     App().listenKeyEvent('keydown','right arrow', moveRight)
+    App().listenKeyEvent('keydown','left arrow', moveLeft)
+    App().listenKeyEvent('keydown','down arrow', moveDown)
+    App().listenKeyEvent('keydown','up arrow', moveUp)
     App().run()
 
 
