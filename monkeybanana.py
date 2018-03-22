d14a572d01628f9092f34a89a750f1b7988331d5
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-03-22 08:19:02 -0400

    Updated from Brython Server: 3/22/2018 8:19:01 AM

--- monkeybanana.py
+++ monkeybanana.py
@@ -1,1 +1,30 @@
+#Liam Collins
+#3/22/18
+#monkeybanana.py - Best game ever
+
+from ggame import *
+
+#constants
+ROWS = 25
+COLS = 50
+CELL_SIZE = 25
+
+def moveRight(event):
+    monkey.x += CELL_SIZE
+
+if __name__ == '__main__':
+    
+    #colors
+    green = Color(0x006600,1)
+    brown = Color(0x8B4513,1)
+    
+    jungleBox = RectangleAsset(CELL_SIZE*COLS,CELL_SIZE*ROWS,LineStyle(1,green),green)
+    monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown),brown)
+    
+    Sprite(jungleBox)
+    monkey = Sprite(monkeyBox)
+    
+    App().listenKeyEvent('keydown','right arrow', moveRight)
+    App().run()
+
 
