994fb4f06e8bd3469d4387b04972e3d361ee9b5a
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-03-23 08:49:20 -0400

    Updated from Brython Server: 3/23/2018 8:49:20 AM

--- monkeybanana.py
+++ monkeybanana.py
@@ -11,6 +11,7 @@ ROWS = 20
 COLS = 40
 CELL_SIZE = 25
 
+#moves monkey right 1 cell if possible
 def moveRight(event):
     if monkey.x < (COLS-1)*CELL_SIZE:
         monkey.x += CELL_SIZE
@@ -18,6 +19,7 @@ def moveRight(event):
             moveBanana()
             updateScore()
 
+#moves monkey left 1 cell if possible
 def moveLeft(event):
     if monkey.x > 0:
         monkey.x -= CELL_SIZE
@@ -25,6 +27,7 @@ def moveLeft(event):
             moveBanana()
             updateScore()
 
+#moves monkey down 1 cell if possible
 def moveDown(event):
     if monkey.y < (ROWS-1)*CELL_SIZE:
         monkey.y += CELL_SIZE
@@ -32,6 +35,7 @@ def moveDown(event):
             moveBanana()
             updateScore()
     
+#moves monkey up 1 cell if possible
 def moveUp(event):
     if monkey.y > 0:
         monkey.y -= CELL_SIZE
@@ -39,22 +43,27 @@ def moveUp(event):
             moveBanana()
             updateScore()
     
+#moves banana to a random location and reset timer
 def moveBanana():
-    data['frames'] = 0
+    data['frames'] = 0 
     banana.x = randint(0,COLS-1)*CELL_SIZE
     banana.y = randint(0,ROWS-1)*CELL_SIZE
 
+#increase score and deplay new text at bottom of screen
 def updateScore():
     data['score'] += 10
     data['scoreText'].destroy() #remove old writing 
     scoreBox = TextAsset('Score = '+str(data['score']))
     data['scoreText'] = Sprite(scoreBox,(0,ROWS*CELL_SIZE))
 
+#keeps track of how many frames have happened and moves banana if it
+#has been more than 100 frams since last move
 def step():
     data['frames'] += 1
     if data['frames']%100 == 0:
         moveBanana()
 
+#sets up and runs the game
 if __name__ == '__main__':
     
     #hold variables in dictionary
@@ -67,6 +76,7 @@ if __name__ == '__main__':
     brown = Color(0x8B4513,1)
     yellow = Color(0xFFFF00,1)
     
+    #graphics
     jungleBox = RectangleAsset(CELL_SIZE*COLS,CELL_SIZE*ROWS,LineStyle(1,green),green)
     monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown),brown)
     bananaBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,yellow),yellow)
