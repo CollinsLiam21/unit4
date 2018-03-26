1ae3196e3e2f1c552f74b2274378cfd81f6274ad
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-03-26 08:41:29 -0400

    Updated from Brython Server: 3/26/2018 8:41:27 AM

--- printSquares.py
+++ printSquares.py
@@ -2,5 +2,11 @@
 #3/19/18
 #printSquares.py
 
-def printSquares(b,h):
-    
+def printSquares(numRows,numCols):
+    for i in range(1,numRows+1):
+        print('+','- - + '*numCols)
+        print('|     '*(numCols+1))
+    print('+','- - + '*numCols)
+
+printSquares(5,3)
+
