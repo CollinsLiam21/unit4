6612b3a375a36c26107ed5beb2dcb80e86a832f0
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-04-05 08:29:05 -0400

    Updated from Brython Server: 4/5/2018 8:29:05 AM

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,32 @@
+#Liam Collins
+#4/5/18
+#quiz4.py
+
+def count(b):
+    for i in range(1,b+1):
+        print(i)
+
+def excitedPrint(string):
+    print(string.upper(),'!!!')
+
+def firstLetter(word):
+    for ch in word:
+        return ch
+        break 
+
+def repeats(a,b,c):
+    if a == b:
+        return True
+    elif a == c:
+        return True
+    elif b == c:
+        return True
+    else:
+        return False
+
+count(4)
+excitedPrint('i love programming')
+print(firstLetter('smeds'))
+print(repeats('smeds',5,'smeds'))
+
 
