8cf39a00864b48d1336338f932c31d909ae6c5ec
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-04-06 08:28:30 -0400

    Updated from Brython Server: 4/6/2018 8:28:29 AM

--- graphicsTicTacToe.py
+++ graphicsTicTacToe.py
@@ -11,23 +11,45 @@ from random import randint
 def mouseClick(event):
     while i<=9:
         if event.x < 150 and event.y < 150:
-        Sprite(x, (25,10))
-    elif event.x < 350 and event.y < 150:
-        Sprite(x, (215,10))
-    elif event.x < 550 and event.y < 150:
-        Sprite(x, (400,10))
-    elif event.x < 150 and event.y < 350:
-        Sprite(x, (25,180))
-    elif event.x < 350 and event.y < 350:
-        Sprite(x, (215,180))
-    elif event.x < 550 and event.y < 350:
-        Sprite(x, (400,180))
-    elif event.x < 150 and event.y < 550:
-        Sprite(x, (25,380))
-    elif event.x < 350 and event.y < 550:
-        Sprite(x, (215,380))
-    elif event.x < 550 and event.y < 550:
-        Sprite(x, (400,380))
+            Sprite(x, (25,10))
+            i += 1
+        elif event.x < 350 and event.y < 150:
+            Sprite(x, (215,10))
+            i += 1
+        elif event.x < 550 and event.y < 150:
+            Sprite(x, (400,10))
+            i += 1
+        elif event.x < 150 and event.y < 350:
+            Sprite(x, (25,180))
+            i += 1
+        elif event.x < 350 and event.y < 350:
+            Sprite(x, (215,180))
+            i += 1
+        elif event.x < 550 and event.y < 350:
+            Sprite(x, (400,180))
+            i += 1
+        elif event.x < 150 and event.y < 550:
+            Sprite(x, (25,380))
+            i += 1
+        elif event.x < 350 and event.y < 550:
+            Sprite(x, (215,380))
+            i += 1
+        elif event.x < 550 and event.y < 550:
+            Sprite(x, (400,380))
+            i += 1
+        n = randint(1-9)
+        if n == 1:
+            Sprite(o, (25,10))
+            i += 1
+        elif n == 2:
+            Sprite(x, (215,10))
+            i += 1
+        elif n == 3:
+            Sprite(x, (400,10))
+            i += 1
+        elif n == 4:
+            Sprite(x, (25,180))
+            i += 1
     
 
 def computerTurn(event):
