4fba80f0a896506091be33cf8f66e700d860bd31
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-04-06 08:50:17 -0400

    Updated from Brython Server: 4/6/2018 8:50:17 AM

--- graphicsTicTacToe.py
+++ graphicsTicTacToe.py
@@ -5,71 +5,57 @@
 from ggame import *
 from random import randint
 
-'''def isEmpty(numSquare):
-    if numSquare == 1:'''
 
 def mouseClick(event):
-    i=0
-    while i<=9:
-        if event.x < 150 and event.y < 150:
-            Sprite(x, (25,10))
-            i += 1
-        elif event.x < 350 and event.y < 150:
-            Sprite(x, (215,10))
-            i += 1
-        elif event.x < 550 and event.y < 150:
-            Sprite(x, (400,10))
-            i += 1
-        elif event.x < 150 and event.y < 350:
-            Sprite(x, (25,180))
-            i += 1
-        elif event.x < 350 and event.y < 350:
-            Sprite(x, (215,180))
-            i += 1
-        elif event.x < 550 and event.y < 350:
-            Sprite(x, (400,180))
-            i += 1
-        elif event.x < 150 and event.y < 550:
-            Sprite(x, (25,380))
-            i += 1
-        elif event.x < 350 and event.y < 550:
-            Sprite(x, (215,380))
-            i += 1
-        elif event.x < 550 and event.y < 550:
-            Sprite(x, (400,380))
-            i += 1
+    if event.x < 150 and event.y < 150:
+        Sprite(x, (25,10))
+        computerTurn()
+    elif event.x < 350 and event.y < 150:
+        Sprite(x, (215,10))
+        computerTurn()
+    elif event.x < 550 and event.y < 150:
+        Sprite(x, (400,10))
+        computerTurn()
+    elif event.x < 150 and event.y < 350:
+        Sprite(x, (25,180))
+        computerTurn()
+    elif event.x < 350 and event.y < 350:
+        Sprite(x, (215,180))
+        computerTurn()
+    elif event.x < 550 and event.y < 350:
+        Sprite(x, (400,180))
+        computerTurn()
+    elif event.x < 150 and event.y < 550:
+        Sprite(x, (25,380))
+        computerTurn()
+    elif event.x < 350 and event.y < 550:
+        Sprite(x, (215,380))
+        computerTurn()
+    elif event.x < 550 and event.y < 550:
+        Sprite(x, (400,380))
+        computerTurn()
     
 
 def computerTurn():
-    if event.x < 1000 and event.y < 1000:
-        n = randint(1,9)
-        if n == 1:
-            Sprite(o, (25,10))
-            i += 1
-        elif n == 2:
-            Sprite(x, (215,10))
-            i += 1
-        elif n == 3:
-            Sprite(x, (400,10))
-            i += 1
-        elif n == 4:
-            Sprite(x, (25,180))
-            i += 1
-        elif n == 5:
-            Sprite(x, (215,180))
-            i += 1
-        elif n == 6:
-            Sprite(x, (400,180))
-            i += 1
-        elif n == 7:
-            Sprite(x, (25,380))
-            i += 1
-        elif n == 8:
-            Sprite(x, (215,380))
-            i += 1
-        elif n == 9:
-            Sprite(x, (400,380))
-            i += 1
+    n = randint(1,9)
+    if n == 1:
+        Sprite(o, (25,10))
+    elif n == 2:
+        Sprite(x, (215,10))
+    elif n == 3:
+        Sprite(x, (400,10))
+    elif n == 4:
+        Sprite(x, (25,180))
+    elif n == 5:
+        Sprite(x, (215,180))
+    elif n == 6:
+        Sprite(x, (400,180))
+    elif n == 7:
+        Sprite(x, (25,380))
+    elif n == 8:
+        Sprite(x, (215,380))
+    elif n == 9:
+        Sprite(x, (400,380))
     
 
 if __name__=='__main__':
